89a5be2a5d935cca3be666f94ffb2cfe018daeba
RochakDhakal <rochakdhakal@gmail.com>
2026-01-14 13:15:13 +0545

    FIX:header-navigation-hover

--- patterns/header-default.php
+++ patterns/header-default.php
@@ -14,12 +14,12 @@
 <!-- wp:group {"style":{"spacing":{"padding":{"top":"24px","bottom":"24px","left":"26px","right":"26px"}}},"backgroundColor":"primary","layout":{"type":"constrained","contentSize":"1180px"}} -->
 <div class="wp-block-group has-primary-background-color has-background" style="padding-top:24px;padding-right:26px;padding-bottom:24px;padding-left:26px"><!-- wp:group {"style":{"spacing":{"padding":{"top":"0","bottom":"0"},"blockGap":"24px","margin":{"top":"0","bottom":"0"}}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
 <div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:0;padding-bottom:0"><!-- wp:group {"className":"is-style-default","style":{"spacing":{"blockGap":"10px"},"typography":{"fontSize":"24px","fontStyle":"normal","fontWeight":"600"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-<div class="wp-block-group is-style-default" style="font-size:24px;font-style:normal;font-weight:600">
-<!-- wp:site-logo {"width":45,"shouldSyncIcon":true} /-->
-<!-- wp:site-title {"style":{"elements":{"link":{"color":{"text":"var:preset|color|light-color"},":hover":{"color":{"text":"var:preset|color|primary"}}}},"typography":{"fontSize":"24px","fontStyle":"normal","fontWeight":"600"}},"textColor":"light-color","fontFamily":"geist"} /--></div>
+<div class="wp-block-group is-style-default" style="font-size:24px;font-style:normal;font-weight:600"><!-- wp:site-logo {"width":45,"shouldSyncIcon":true} /-->
+
+<!-- wp:site-title {"style":{"elements":{"link":{"color":{"text":"var:preset|color|light-color"},":hover":{"color":{"text":"var:preset|color|secondary"}}}},"typography":{"fontSize":"24px","fontStyle":"normal","fontWeight":"600"}},"textColor":"light-color","fontFamily":"geist"} /--></div>
 <!-- /wp:group -->
 
-<!-- wp:navigation {"textColor":"light-color","className":"is-style-relyo-navigation-secondary","style":{"typography":{"fontStyle":"normal","fontWeight":"500","fontSize":"16px"}}} -->
+<!-- wp:navigation {"textColor":"light-color","customOverlayTextColor":"#010e11","className":"is-style-relyo-navigation-secondary","style":{"typography":{"fontStyle":"normal","fontWeight":"500","fontSize":"16px"}}} -->
 	<!-- wp:home-link /-->
 	<!-- wp:page-list /-->
 <!-- /wp:navigation -->
